3b26d7039806d6a29ddacdf04251a1859474a856
Richi Jeffery <richi1717@gmail.com>
2020-05-23 15:21:30 -0700

    clean up [battleScene].js

--- src/pages/battle/[battleScene].js
+++ src/pages/battle/[battleScene].js
@@ -1,59 +1,19 @@
-import styled, { createGlobalStyle } from 'styled-components'
-import PropTypes from 'prop-types'
 // import useSWR from 'swr'
 import { random, sampleSize } from 'lodash'
 import fetch from 'node-fetch'
+import PropTypes from 'prop-types'
+import { BattleContainer, Container, GlobalStyle } from '../../pagesStyled'
+import { characterMutations } from '../../operations/mutations'
+import { normalBattleMusic } from '../../helpers/soundEffects'
 import {
   DATA_BASE_URL_CHARACTERS,
   DATA_BASE_URL_MONSTERS,
 } from '../../constants/databaseUrls'
-import Heroes from '../../components/Heroes'
 import Enemies from '../../components/Enemies'
+import Heroes from '../../components/Heroes'
 import StatusWindow from '../../components/StatusWindow'
-import { characterMutations } from '../../operations/mutations'
-import { normalBattleMusic } from '../../helpers/soundEffects'
 
 const battleScenes = ['boss', 'beach', 'desert', 'forest', 'grass'] // use to redirect to different tests
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  color: white;
-  background-image: url("/img/${(props) => props.battleScene}-battle.png");
-  background-size: 50px;
-  max-width: 1352px;
-  height: 100vh;
-  width: 100vw;
-  background-size: cover;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-`
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-  }
-  html {
-  font-family: 'Orbitron', sans-serif;
-  box-sizing: border-box;
-  color: transparent;
-  font-size: 10px; }
-  *, *::before, *::after {
-  box-sizing: inherit;
-  color: inherit;
-  }
-
-body {
-  margin: 0;
-  background-color: #212527; }
-`
-
-const BattleContainer = styled.div`
-  flex: 1;
-  height: 500px;
-  display: flex;
-`
 
 export default function Location ({ battleScene, enemies, heroes }) {
   characterMutations.addCharacters(enemies, heroes)
---
+++ src/pagesStyled/index.js
@@ -0,0 +1,43 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  color: white;
+  background-image: url("/img/${(props) => props.battleScene}-battle.png");
+  background-size: 50px;
+  max-width: 1352px;
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+  html {
+    font-family: 'Orbitron', sans-serif;
+    box-sizing: border-box;
+    color: transparent;
+    font-size: 10px;
+  }
+  *, *::before, *::after {
+    box-sizing: inherit;
+    color: inherit;
+    }
+
+  body {
+    margin: 0;
+    background-color: #212527;
+  }
+`
+
+export const BattleContainer = styled.div`
+  flex: 1;
+  height: 500px;
+  display: flex;
+`
