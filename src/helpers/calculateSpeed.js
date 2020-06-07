04fe2c6846972e622243faede30d1cb6d326149d
Richi Jeffery <richi1717@gmail.com>
2020-06-07 01:24:58 -0700

    removed unused files

--- src/helpers/calculateSpeed.js
+++
@@ -1,3 +0,0 @@
-export const getAgility = (agility = 1) => 1000 - agility * 8
-
-export default getAgility
--- src/helpers/time-out.js
+++
@@ -1,5 +0,0 @@
-export default function setTimeOutHelper(time, func, arg1, arg2) {
-  return setTimeout(() => {
-    func(arg1, arg2);
-  }, time);
-}
