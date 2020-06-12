9dddebddb61ec21f0236f17c5c36a5d14a9b734e
Richi Jeffery <richi1717@gmail.com>
2020-06-12 01:13:42 -0700

    add victory condition, music, and message

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,23 @@
 import { useQuery } from '@apollo/client'
+import { useEffect } from 'react'
 import { InfoBar } from './styled'
 import { GET_WHOSE_TURN } from '../../operations/queries/getWhoseTurn'
+import { GET_ENEMIES } from '../../operations/queries/getCharacters'
+import { battleVictoryMusic } from '../../helpers/soundEffects'
 
 export default function Header () {
   const whoseTurnQuery = useQuery(GET_WHOSE_TURN)
+  const enemiesQuery = useQuery(GET_ENEMIES)
   const whoseTurn = whoseTurnQuery?.data?.whoseTurn
+  const enemies = enemiesQuery?.data?.enemies
+
+  useEffect(() => {
+    if (!enemies.length) {
+      battleVictoryMusic()
+    }
+  }, [enemies.length])
+
+  if (!enemies.length) return <InfoBar>You Win!</InfoBar>
 
   return whoseTurn?.name ? <InfoBar>{whoseTurn?.name}'s turn</InfoBar> : null
 }
